62907ba624f3d4942ad190d123f028b0a062b828
Vaporise <draxdragonfire@googlemail.com>
2024-11-06 21:22:07 +0900

    small update on htmlnode code

---
+++ src/htmlnode.py
@@ -0,0 +1,15 @@
+
+
+
+
+class HTMLnode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+
+    
+    def __to_html__(self):
+        raise NotImplementedError
+
+    def __props_to_html(self):
+
+    
+    def __repr__(self):
